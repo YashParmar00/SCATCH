ba68aadbe37e612adc3d86f8dd619be9cf9ab070
YashParmar00 <yashsingh0033@gmail.com>
2025-04-14 22:03:41 +0530

    Add Add to cart, login, logout, and many more new routes and functionallity

---
+++ routes/index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const router = express.Router();
+const isLoggedin = require('../middlewares/isLoggedIn');
+const productModel = require('../models/product-model');
+const userModel = require('../models/user-model');
+
+router.get("/", (req, res) => {
+    let error = req.flash("error");
+    res.render("index", { error, loggedin: false });
+});
+
+router.get("/shop", isLoggedin, async (req, res) => {
+    let products = await productModel.find();
+    let success = req.flash("success")
+    res.render("shop", { success, products });
+});
+
+router.get("/cart", isLoggedin, async (req, res) => {
+    let user = await userModel.findOne({email: req.user.email}).populate("cart");
+    
+    let bill = (Number(user.cart[0].price)+20)-Number(user.cart[0].discount);
+    
+    
+    res.render("cart", { user, bill });
+});
+
+router.get("/addtocart/:productid", isLoggedin, async (req, res) => {
+  let user = await userModel.findOne({email: req.user.email});
+  user.cart.push(req.params.productid);
+  await user.save();
+  req.flash("success", "Added to cart");
+  res.redirect("/shop");
+});
+
+router.get("logout", isLoggedin, (req, res) => {
+    res.render("shop");
+});
+
+module.exports = router;
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,8 +1,15 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const { registerUser, loginUser, logout } = require('../controllers/authController');
 
-router.get('/', (req, res) => {
-    res.send("Welcome user");
-}); 
+router.get("/", (req, res) => {
+  res.send("Welcome user");
+});
 
-module.exports = router ;
+router.post("/register", registerUser);
+
+router.post('/login', loginUser);
+
+router.get("/logout", logout);
+
+module.exports = router;
