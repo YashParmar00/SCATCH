5977200420db17c0e5188ce06477eee388ab8b82
YashParmar00 <yashsingh0033@gmail.com>
2025-04-15 11:19:11 +0530

    add new funtionality and navbar functionality.

--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -1,32 +1,28 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const ownerModel = require('../models/owner-model');
+const ownerModel = require("../models/owner-model");
 
+router.post("/create", async (req, res) => {
+  let owners = await ownerModel.find();
+  if (owners.length > 0) {
+    return res
+      .status(503)
+      .send("You don't have permission to create a new owner.");
+  }
 
+  let { fullname, email, password } = req.body;
 
-    router.post('/create', async (req, res) => {
-       let owners = await ownerModel.find();
-       if(owners.length > 0){
-         return res.status(503).send("You don't have permission to create a new owner.");
-       }
+  let createdOwner = await ownerModel.create({
+    fullname,
+    email,
+    password,
+  });
+  res.status(201).send(createdOwner);
+});
 
-       let {fullname, email, password} = req.body ;
-
-      let createdOwner = await ownerModel.create({
-        fullname,
-        email,
-        password,
-       });
-       res.status(201).send(createdOwner);
-    });
-
-
-router.get('/admin', (req, res) => {
+router.get("/admin", (req, res) => {
   let success = req.flash("success");
   res.render("createproducts", { success });
-}); 
+});
 
-
-
-
-module.exports = router ;
+module.exports = router;
